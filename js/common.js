f4f7ac45bef86565af459a0c4076f072077e750d
henry <hoon3977@naver.com>
2019-04-17 22:58:12 +0900

    이미지 경로 수정

--- js/common.js
+++
@@ -1,9 +0,0 @@
-function getRequest(key) {
-    var arr = decodeURI(location.search).replace('?', '').split('&')
-    for (let index = 0; index < arr.length; index++) {
-        if (key == arr[index].split('=')[0]) {
-            return arr[index].split('=')[1]
-        }
-    }
-    return ''
-}
